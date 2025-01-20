0b3db8bd11da1ea63220946dc355f0b7ac9dfee7
peppermint100 <krpeppermint100@gmail.com>
2025-01-20 14:50:46 +0900

    Get Templates By userId

--- template-service/src/main/java/com/peppermint100/templateservice/controller/TemplateController.java
+++ template-service/src/main/java/com/peppermint100/templateservice/controller/TemplateController.java
@@ -1,6 +1,7 @@
 package com.peppermint100.templateservice.controller;
 
 import com.peppermint100.templateservice.dto.TemplateDto;
+import com.peppermint100.templateservice.service.TemplateQueryService;
 import com.peppermint100.templateservice.service.TemplateService;
 import com.peppermint100.templateservice.vo.CreateTemplateRequest;
 import com.peppermint100.templateservice.vo.GlobalResponse;
@@ -11,7 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/templates")
@@ -20,10 +23,24 @@ public class TemplateController {
 
     private final TemplateService templateService;
 
+    @GetMapping
+    public ResponseEntity<GlobalResponse> getTemplatesByUserId(@RequestParam("userId") UUID userId) {
+        List<TemplateDto> templateDtoList = templateService.getTemplatesByUserId(userId);
+
+        List<TemplateVO> templateVOs = templateDtoList.stream().map(TemplateVO::of).collect(Collectors.toList());
+        GlobalResponse<List<TemplateVO>> response = GlobalResponse.<List<TemplateVO>>builder()
+                .message("템플릿 목록을 가져왔습니다.")
+                .data(templateVOs)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @PostMapping
     public ResponseEntity<GlobalResponse> addTemplate(@RequestBody  CreateTemplateRequest request) {
+        UUID userId = request.getUserId();
         String name = request.getName();
-        TemplateDto templateDto = templateService.addTemplate(name);
+        TemplateDto templateDto = templateService.addTemplate(userId, name);
         TemplateVO templateVO = TemplateVO.of(templateDto);
         GlobalResponse<TemplateVO> response = GlobalResponse.<TemplateVO>builder().message("템플릿을 추가했습니다.").data(templateVO).build();
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
--- template-service/src/main/java/com/peppermint100/templateservice/service/TemplateService.java
+++ template-service/src/main/java/com/peppermint100/templateservice/service/TemplateService.java
@@ -2,12 +2,15 @@ package com.peppermint100.templateservice.service;
 
 import com.peppermint100.templateservice.dto.TemplateDto;
 import com.peppermint100.templateservice.entity.Template;
+import com.peppermint100.templateservice.exception.TemplateDoesNotExistsException;
 import com.peppermint100.templateservice.repository.TemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +18,13 @@ public class TemplateService {
 
     private final TemplateQueryService templateQueryService;
     private final TemplateRepository templateRepository;
+
+    public List<TemplateDto> getTemplatesByUserId(UUID userId) {
+        return templateQueryService.getTemplatesByUserId(userId)
+                .stream()
+                .map(TemplateDto::of)
+                .collect(Collectors.toList());
+    }
 
     public TemplateDto addTemplate(UUID userId, String name) {
         Template template = Template.of(userId, name);
