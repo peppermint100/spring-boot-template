e50c9ef4facf8eb4663e402d98a82e489c7b0f6d
peppermint100 <krpeppermint100@gmail.com>
2025-01-21 14:53:35 +0900

    Created user entity, user table with flyway

---
+++ user-service/src/main/java/com/peppermint100/userservice/entity/ApplicationUser.java
@@ -0,0 +1,39 @@
+package com.peppermint100.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApplicationUser {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @UuidGenerator @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
